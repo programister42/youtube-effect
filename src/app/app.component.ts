843e56308605ca853cc7e18ec1a24badb0c6db40
Taras Tereschenko <tarrrasio@gmail.com>
2024-08-23 19:12:04 +0500

    feat: setup app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
 
 @Component({
-  selector: 'ye-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+	selector: "ye-root",
+	standalone: true,
+	imports: [RouterOutlet],
+	templateUrl: "./app.component.html",
+	styleUrl: "./app.component.scss",
 })
 export class AppComponent {
-  title = 'youtube-effect';
+	title = "youtube-effect";
 }
